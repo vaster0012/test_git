f1d2785769ea68d0fc3bca379cdd59797af191d8
vaster0012 <_email@gmail.com>
2026-05-03 00:08:25 +0500

    add more st

--- health_check.sh
+++ health_check.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+RED="\e[31m"
+GRN="\e[32m"
+EC="\e[0m"
+
+echo -e "${RED}Тест скрипта проверки здоровья"
+
+    echo -e "Дата отчета:\n"
+    date
+
+    echo -e "\n uptime:\n"
+    uptime
+aCPU="$(top -bn1 | grep "%Cpu" | awk '{print $2}')"
+echo -e "Загрузка CPU"
+echo $(top -bn1 | grep %Cpu | cut -d: -f2-)
+if [ $(bc <<< "$aCPU > 40") -eq 1 ]
+    then echo OK
+    else echo CHECK PROCESS!
+fi 
