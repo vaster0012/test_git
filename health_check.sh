420aa7e237333b541131e41360cf1eb67e00bf7d
vaster0012 <_email@gmail.com>
2026-05-03 01:09:47 +0500

    minor update

--- health_check.sh
+++ health_check.sh
@@ -4,17 +4,17 @@ RED="\e[31m"
 GRN="\e[32m"
 EC="\e[0m"
 
-echo -e "${RED}Тест скрипта проверки здоровья"
+echo -e "${RED}Тест скрипта проверки здоровья${EC}"
 
     echo -e "Дата отчета:\n"
     date
 
-    echo -e "\n uptime:\n"
-    uptime
+    echo -e "\n uptime: $(uptime | cut -d, -f1 | awk '{print $3}')"
+
 aCPU="$(top -bn1 | grep "%Cpu" | awk '{print $2}')"
-echo -e "Загрузка CPU"
+echo -e "\n Загрузка CPU"
 echo $(top -bn1 | grep %Cpu | cut -d: -f2-)
 if [ $(bc <<< "$aCPU > 40") -eq 1 ]
-    then echo OK
-    else echo CHECK PROCESS!
+    then echo -e "${RED} ${aCPU} !CHECK PROCESS!${EC}"
+    else echo -e "${GRN} ${aCPU} OK! ${EC}"
 fi 
