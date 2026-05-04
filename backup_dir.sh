ae843e63514f40c1a367097326c415794e0029a6
vaster0012 <vaster0012@gmail.com>
2026-05-04 21:23:26 +0000

    add BACKUP script

--- backup_dir.sh
+++ backup_dir.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
-#перебор чисел, пока не остановится на определенном. while do done
 
-min_num=1
-max_num=9
-step=1
+mkdir -p "./backup/$(date +%Y)"
+touch ./backup/tar.log
 
-while [ "$min_num" -lt "$max_num" ]
-    do
-        echo "$min_num"
-        min_num=$(( min_num + step ))
+while getopts "d:" opt
+do
+    case "$opt" in
+        d) DIR="$OPTARG" ;;
+        *) echo "Non supported key" ;;
+    esac
 done
 
-for num in $(seq 1 1 9)
-    do
-        echo "$num"
-done
+if [ -d ${DIR} ]
+    then 
+        echo "${DIR} - found and is a directory"
+        echo -e "${DIR}: start archive\e[32m"
+        if [ -e "./backup/$(date +%Y)/${DIR}-$(date +%d.%m).tar.gz" ]
+            then
+                rm -rf "./backup/$(date +%Y)/${DIR}-$(date +%d.%m).tar.gz"
+                tar -czvf "./backup/$(date +%Y)/${DIR}-$(date +%d.%m).tar.gz" "${DIR}"
+                echo -e "Rewrite ARCHIVE: ${DIR}-$(date +%d.%m).tar.gz" | tee ./backup/tar.log
+                echo -e "\e[0m Archive is rewrite!"
+            else
+                tar -czvf "./backup/$(date +%Y)/${DIR}-$(date +%d.%m).tar.gz" "${DIR}"
+                echo -e "Create ARCHIVE: ${DIR}-$(date +%d.%m).tar.gz" | tee ./backup/tar.log
+        fi  
+    else
+    echo "${DIR} - not found or is not a directory"  
+fi
+
+
+
+echo " directive: ${DIR} "
+
+
+
+
+
+
+#case "$1" in
+#    -d echo Direcory
+#    echo $1 not a supported
+#esac
