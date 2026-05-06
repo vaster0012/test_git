76dde45b4db627c9b12a38473e752e8be5b4d593
vaster0012 <vaster0012@gmail.com>
2026-05-06 19:30:37 +0000

    v2 skeleton

--- newtest.sh
+++ newtest.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-#colors
-RED="\e[31m"
-GRN="\e[32m"
-YEL="\e[33m"
-EC="\e[0m"
+    #colors
+RED="\e[31m"   # Красный цвет текста
+GRN="\e[32m"   # Зелёный цвет текста
+YEL="\e[33m"   # Жёлтый цвет текста
+EC="\e[0m"     # Сброс цвета
 
 
 printf "Мониторинг сайтов\n"
-
+LINK=$(cat site.txt | column -t -s "//" | awk '{print $2}') || echo "nofile"
 
 while getopts "a" opt
 do
     case "$opt" in
-        a) cat ./site.txt || printf "File deleted or is damage"
+        a) cat ./site.txt || printf "File deleted or is damage\n"
            exit 0 ;;
-        *) echo "Non supported key. Run." ;;
+        *) echo "Unsupported key. Running without flags" ;;
     esac
 done
 
 if [ -s "./site.txt" ]
     then
-        printf "OK. Cheking list" #change
+        printf "OK. Cheking list\n" #change
+        echo $(LINK)
     else
         printf "File doesn't exist or is empty\n" #change
         printf "Create site.txt\n"
+        printf "Please! Input site in site!))\n"
         touch ./site.txt #change
+        exit 0
 fi
 
+for LINK in $(cat ./site.txt)
+    do
+    $(ping -c 1 $LINK)
+done
