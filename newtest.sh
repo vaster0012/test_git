5a5c5d41d0a919cab6ea591948b6265624cebc22
vaster0012 <vaster0012@gmail.com>
2026-05-08 21:06:35 +0000

    update monitor

--- newtest.sh
+++ newtest.sh
@@ -6,6 +6,7 @@ GRN="\e[32m"   # Зелёный цвет текста
 YEL="\e[33m"   # Жёлтый цвет текста
 EC="\e[0m"     # Сброс цвета
 DEF_SITE="./site.txt" # стандартный файл
+DEF_LINK="8.8.8.8"
 
 printf "${GRN}Мониторинг сайтов${EC}\n"
 
@@ -14,7 +15,7 @@ monitor () {
         if [ -s "$SITE" ]
         then
             printf "${GRN}OK${EC}. Cheking list\n"
-            if ping -c 1 -w 2 8.8.8.8 > /dev/null 2>&1; then 
+            if ping -c 1 -w 2 $DEF_LINK > /dev/null 2>&1; then 
                     printf "${GRN}CONNECTED${EC}\n" # change later
                     #cat ${SITE} | column -t -s "//" | awk '{print $2}' || echo "nofile"
                 else
@@ -41,14 +42,56 @@ monitor () {
     done < "$SITE"
 }
 
-while getopts "f:h" opt   #СДЕЛАТЬ ПРОВЕРКУ ОДНОГО САЙТА ПО КЛЮЧУ И ЕГО параметру. А так же можно улучшить
-do                        #проверяя дополнительные параметры
-    case "$opt" in        #например: ... (пока хз)
-        f) U_SITE=$OPTARG
-           touch ${U_SITE} 
-           monitor "$U_SITE" ;;
-        h) echo "add the key -f <NAME> to create or specify the file " ;;
-        *) echo "Unsupported key. Running without flags" ;;
+one_site () {
+    local LINK=${1:-$DEF_LINK}
+    printf "\n=== $(date) ===\n" | tee -a "./smonitor.log"
+    printf "Test one site - $LINK"
+    while IFS= read -r LINK
+    do
+        [[ -z "$LINK" || "$LINK" == \#* ]] && continue
+        CODE=$(curl -o /dev/null -s -m 3 -w "%{http_code}" "$LINK")
+        if [ "$CODE" -ge 200 ] && [ "$CODE" -lt 400 ]
+            then
+                printf "${GRN}OK${EC} $LINK (HTTP $CODE)\n" | tee -a "./smonitor.log"
+            else
+                printf "${RED}FAIL${EC} $LINK (HTTP $CODE)\n" | tee -a "./smonitor.log"
+        fi
+    done < "$SITE"
+
+
+}
+
+help_page () {
+    #ОПИСАНИЕ И ПОМОЩЬ ПО ФЛАГАМ
+    #ПО ЭТОМУ СКРИПТУ
+    printf "\n Скрипт позволяет просто пингануть сайты "
+    printf "\n Флаги: "
+    printf "\n -h --- Помощь "
+    printf "\n -f --- Задать свой файл дял проверки "
+    printf "\n -s --- Задать только сайт для проверки П.с. Работает пока криво \n"
+    printf "\n Просьба использовать только один флаг "
+    printf "\n Это все тесты, если вдруг кто-то это увидит, простите, что увидели! \n"
+
+}
+
+while getopts "f:hs:" opt
+do  
+    if [ -n "$FLAG"  ] 
+        then 
+            echo -e "Error: ${YEL}KEY -$opt ${EC} conflicts with already used key -$FLAG"
+            exit 1
+    fi
+    case "$opt" in        
+        f)  FLAG=f
+            U_SITE=$OPTARG
+            touch ${U_SITE} 
+            monitor "$U_SITE" ;;
+        h)  FLAG=h
+            help_page ;;
+        s)  FLAG=s
+            L_SITE=$OPTARG 
+            one_site "L_SITE" ;;
+        *)  echo "Unsupported key. Running without flags" ;;
     esac
 done
 
