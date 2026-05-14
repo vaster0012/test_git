c58c5dd73410297a44daea089ead09588a892185
vaster0012 <vaster0012@gmail.com>
2026-05-14 21:01:27 +0000

    more add. Add setup GIT

--- setup_enviroment.sh
+++ setup_enviroment.sh
@@ -37,6 +37,11 @@ if [[ -s ./packages.txt ]]
 fi
 curl -s -L -o ./packages.txt "https://raw.githubusercontent.com/vaster0012/test_git/refs/heads/main/packages.txt"
 
+log () {
+    echo "[$(date '+%m.%d.%Y %H:%M:%S')] $1" >> enviroment.log
+}
+log "Start script!" 
+
 title_bar () {
     local MESG=$1
     printf '%s' "${YEL}"   # Авто построение бара
@@ -56,12 +61,16 @@ cleanup() { # обработка trap
             printf '%s\n' "ENV║                                   OK ║ "
             printf '%s\n' "ENV╚══════════════════════════════════════╝ " 
             printf '%s' "${EC}"  
+            log "End OK!"
+            log "       "
         else
             printf '%s' "${YEL}"   # Все закончилось плохо
             printf '%s\n' "ENV║ ⇓ ⇓ ╚════════════════════════════════╗ "
             printf '%s\n' "ENV║                                  BAD ║ "
             printf '%s\n' "ENV╚══════════════════════════════════════╝ " 
             printf '%s' "${EC}"  
+            log "End ERROR!"
+            log "       "
     fi
 }
 
@@ -73,7 +82,7 @@ help_page () {  #Страница помощи
     printf '%s\n' "${HST}     -u --- Обновить только установленные "
     printf '%s\n' "${HST}     Просьба использовать только один флаг "
     printf '%s\n' "${HST}     Это все тесты, если вдруг кто-то это увидит, простите, что увидели! (С)Vaster "
-
+    log "Open help"
 }
 
 printf '%s\n' "${ST} A temporary file has been created"
@@ -87,15 +96,23 @@ check_all_installed () { # только провека установлено л
     printf '%s\n' "${ST}   ...Please wait"
     #apt-get update &>/dev/null
 
+    OV_COUNT=0
+    NO_INST_COUNT=0
     while IFS= read -r number
     do
         if grep -qxF "$number" "./allinst.tmp"
         then 
+            OV_COUNT=$((OV_COUNT + 1))
             printf '%s\n' "${GST} $number installed!"
         else 
+            NO_INST_COUNT=$((NO_INST_COUNT + 1))
              printf '%s\n' "${EST}${RED} $number not install!${EC}"
         fi
     done < <(grep -v '^ *#' "./packages.txt")
+    printf '%s' "${ST} $((OV_COUNT + NO_INST_COUNT)) total PACK | "
+    printf '%s' "${GRN} $OV_COUNT ${EC} Already installed | "
+    printf '%s\n' "${RED} $NO_INST_COUNT ${EC} of them are not installed"
+    log "TOTAL $((OV_COUNT + NO_INST_COUNT)). INSTALL $OV_COUNT. NOT INTALL $NO_INST_COUNT."
 }   
 
 installation () { # в работе. Установка требуемых пакетов
@@ -112,11 +129,12 @@ installation () { # в работе. Установка требуемых па�
         then 
             printf '%s\n' "${GST} $PACKAGE installed!"
         else 
-            sudo DEBIAN_FRONTEND=noninteractive apt install -y \
+            sudo DEBIAN_FRONTEND=noninteractive apt install -y "$PACKAGE"\
                         -o Dpkg::Options::="--force-confdef" \
                         -o Dpkg::Options::="--force-confold" \
                         пакет > /dev/null 2>&1
             title_bar "INSTALL $PACKAGE"
+            log "INST $PACKAGE"
             printf '%s\n' "${ST} Installing ${YEL}$PACKAGE${EC}"
             printf '%s\n' "${ST} Please wait!"
 
@@ -124,11 +142,20 @@ installation () { # в работе. Установка требуемых па�
     done < <(grep -v '^ *#' "./packages.txt")
 }
 
+install_zsh() { 
+    sh -c "$(curl -sSL https://github.com/vaster0012/first-config-zsh/raw/refs/heads/main/stab/stableinst.sh)"
+    }
+
 git_env_intallation () {
     printf '%s\n' "${ST} IN PROGRES...."
-
+    mkdir projet && cd projet
+    git config -- global user.name "vaster0012"
+    git config -- global user.email "vaster0012@gmail.com"
+    git clone git@github.com:vaster0012/test_git.git
+    git fetch origin
+    git pull origin main
+    cd ~
 }
-
 
 check_box_title () {
     export TERM=xterm
@@ -140,32 +167,38 @@ check_box_title () {
     title=yellow,black'
 
     whiptail --topleft --title "Updating and installing customized packages" \
-        --menu "SELECT NEXT STEP TO Setup bootstrap Vaster" 14 48 3 \
+        --menu "SELECT NEXT STEP TO Setup bootstrap Vaster" 14 48 4 \
     "1" "FULL INSTALL" \
     "2" "ONLY PACKAGES" \
-    "3" "MORE OPTION" 2> /tmp/ans
+    "3" "MORE OPTION" \
+    "4" "EXIT" 2> /tmp/ans
     case $(cat /tmp/ans) in
         1) title_bar "SELECTED FULL INSTALL"
-            installation ;;
+            installation
+            git_env_intallation 
+            install_zsh   ;;
         2) title_bar "SELECTED ONLY PACKAGES" ;;
         3) title_bar "MORE OPTION" 
             check_box_more;;
+        4) exit 1 ;;
     esac
 }
 check_box_more () {
  whiptail --topleft --title "Updating and installing customized packages" \
-        --menu "More option" 14 48 3 \
+        --menu "More option" 14 48 4 \
     "1" "CHECK INSTALLED APT" \
     "2" "INSTALL ONLY ZSH" \
-    "3" "TEST3" 2> /tmp/ans
+    "3" "INSTALL ONLY GIT" \
+    "4" "BACK" 2> /tmp/ans
     case $(cat /tmp/ans) in
         1) check_all_installed ;;
         2) title_bar "INSTALL ONLY ZSH"
-        sh -c "$(curl -sSL https://github.com/vaster0012/first-config-zsh/raw/refs/heads/main/stab/stableinst.sh)" ;;
-        3) title_bar "TEST3" ;;
+            install_zsh ;;
+        3) git_env_intallation ;;      
+        4) check_box_title ;;
     esac
 }
-check_box_title
+
 FLAG=""
 while getopts "has" opt; do
     if [[ -n "$FLAG" ]]; then
@@ -184,11 +217,13 @@ done
 
 case "$FLAG" in
     h) help_page ;;
-    a) check_all_installed ;;
+    a) check_all_installed 
+        exit 1;;
     s)  check_box_title
         printf '%s\n' "${EST}not ready ...." ;;
 esac
 
 shift $((OPTIND - 1))
-
+check_box_title
+log "EXIT WHITOUT TRAP"
 trap cleanup EXIT
